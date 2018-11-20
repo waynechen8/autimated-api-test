34f26d048d62f4b1d7f8b7411962f1aa4f3b3c7c
Wayne <Wayne4_Chen@asus.com>
2018-11-20 18:28:30 +0800

    soc success except delete, OOO ok

---
+++ OOO/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Run(Mount Volume, Stress)
+
+#Create
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jtl
+
+chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem
+remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/OOO/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
+ssh -i /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem ubuntu@$remoteVmIp << EOF
+  sudo apt-get update
+  sudo apt-get -y install stress git
+
+  sudo mkfs.ext4 /dev/vdb
+  sudo mkdir /home/ubuntu/volume
+  sudo mount /dev/vdb  /home/ubuntu/volume
+
+  sudo git clone https://github.com/tensorflow/tensorflow.git /home/ubuntu/volume/tensorflow
+
+  stress -c 16 --timeout 10m --backoff 100000000
+
+  exit
+EOF
+
+#Run(JMX)
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jtl
+
+#Stop
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jtl
+
+sleep 2m
+
+#Resume
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jtl
+
+sleep 2m
+
+#Delete
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jtl
