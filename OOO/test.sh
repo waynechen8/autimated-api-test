7a5965f70bf59b2252b56f6a9284903980ddc3a5
Wayne <Wayne4_Chen@asus.com>
2018-11-22 19:30:07 +0800

    disable create_network and delete_network, no vm login and stress cpu

--- OOO/test.sh
+++ OOO/test.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 #Run(Mount Volume, Stress)
 
+#Delete exist result
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/OOO/Create_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/OOO/Run_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/OOO/Stop_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/OOO/Resume_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/OOO/Delete_Result.xml
+
 #Create
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jtl
 
-python3 jmetertodb.py -xmlfile ./Create_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/OOO/Create_Result.xml
 
-chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem
-remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/OOO/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
+
+#chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem
+#remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/OOO/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
+:'
 ssh -i /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem ubuntu@$remoteVmIp << EOF
   sudo apt-get update
   sudo apt-get -y install stress git
@@ -22,30 +31,31 @@ ssh -i /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem ubuntu@$remoteVmIp << E
 
   exit
 EOF
+'
 
 #Run(JMX)
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jtl
 
 sleep 3m
 
-python3 jmetertodb.py -xmlfile ./Run_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/OOO/Run_Result.xml
 
 #Stop
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jtl
 
 sleep 5m
 
-python3 jmetertodb.py -xmlfile ./Stop_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/OOO/Stop_Result.xml
 
 #Resume
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jtl
 
 sleep 5m
 
-python3 jmetertodb.py -xmlfile ./Resume_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/OOO/Resume_Result.xml
 
 #Delete
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jtl
 
-python3 jmetertodb.py -xmlfile ./Delete_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/OOO/Delete_Result.xml
 
--- SOC/test.sh
+++ SOC/test.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 #Run(Mount Volume, Stress)
 
+#Delete exist result
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/SOC/Create_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/SOC/Run_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/SOC/Stop_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/SOC/Resume_Result.xml
+rm -f /home/jmeter/NCHC_Swagger/WL-A01/SOC/Delete_Result.xml
+
 #Create
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Create.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Create.jtl
 
-python3 jmetertodb.py -xmlfile ./Create_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/SOC/Create_Result.xml
 
-chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem
-remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/SOC/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
-ssh -i /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem ubuntu@$remoteVmIp << EOF
+#chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem
+#remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/SOC/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
+:'ssh -i /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem ubuntu@$remoteVmIp << EOF
   sudo apt-get update
   sudo apt-get -y install stress git
 
@@ -22,26 +29,26 @@ ssh -i /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem ubuntu@$remoteVmIp << E
 
   exit
 EOF
-
+'
 #Run(JMX)
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Run.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Run.jtl
 
 sleep 3m 
-python3 jmetertodb.py -xmlfile ./Run_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/SOC/Run_Result.xml
 
 #Stop
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Stop.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Stop.jtl
 
 sleep 5m
-python3 jmetertodb.py -xmlfile ./Stop_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/SOC/Stop_Result.xml
 
 #Resume
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Resume.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Resume.jtl
 
 sleep 5m
-python3 jmetertodb.py -xmlfile ./Resume_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/SOC/Resume_Result.xml
 
 #Delete
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Delete.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Delete.jtl
 
-python3 jmetertodb.py -xmlfile ./Delete_Result.xml
+python3 jmetertodb.py -xmlfile /home/jmeter/NCHC_Swagger/WL-A01/SOC/Delete_Result.xml
