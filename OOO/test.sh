28eea48f4affee16bf92eafc33f50a5b1b4d0e98
Wayne <Wayne4_Chen@asus.com>
2018-11-22 14:33:40 +0800

    fix SOC delete jmx csvconfig path, add delete asp on a server api

--- OOO/test.sh
+++ OOO/test.sh
@@ -4,6 +4,8 @@
 #Create
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Create.jtl
 
+python3 jmetertodb.py -xmlfile ./Create_Result.xml
+
 chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem
 remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/OOO/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
 ssh -i /home/jmeter/NCHC_Swagger/WL-A01/OOO/keypairs.pem ubuntu@$remoteVmIp << EOF
@@ -24,15 +26,26 @@ EOF
 #Run(JMX)
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Run.jtl
 
+sleep 3m
+
+python3 jmetertodb.py -xmlfile ./Run_Result.xml
+
 #Stop
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Stop.jtl
 
-sleep 2m
+sleep 5m
+
+python3 jmetertodb.py -xmlfile ./Stop_Result.xml
 
 #Resume
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Resume.jtl
 
-sleep 2m
+sleep 5m
+
+python3 jmetertodb.py -xmlfile ./Resume_Result.xml
 
 #Delete
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/OOO/WL-A01_OOO_Delete.jtl
+
+python3 jmetertodb.py -xmlfile ./Delete_Result.xml
+
--- SOC/test.sh
+++ SOC/test.sh
@@ -4,6 +4,8 @@
 #Create
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Create.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Create.jtl
 
+python3 jmetertodb.py -xmlfile ./Create_Result.xml
+
 chmod 400 /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem
 remoteVmIp="$(cat /home/jmeter/NCHC_Swagger/WL-A01/SOC/wla01.properties | sed -n '3p' | cut -d "=" -f2)"
 ssh -i /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem ubuntu@$remoteVmIp << EOF
@@ -16,7 +18,7 @@ ssh -i /home/jmeter/NCHC_Swagger/WL-A01/SOC/keypairs.pem ubuntu@$remoteVmIp << E
 
   sudo git clone https://github.com/tensorflow/tensorflow.git /home/ubuntu/volume/tensorflow
 
-  stress -c 16 --timeout 10m --backoff 100000000
+  stress -c 16 --timeout 40m --backoff 600000000
 
   exit
 EOF
@@ -24,9 +26,22 @@ EOF
 #Run(JMX)
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Run.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Run.jtl
 
+sleep 3m 
+python3 jmetertodb.py -xmlfile ./Run_Result.xml
+
 #Stop
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Stop.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Stop.jtl
+
+sleep 5m
+python3 jmetertodb.py -xmlfile ./Stop_Result.xml
 
 #Resume
 /home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Resume.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Resume.jtl
 
+sleep 5m
+python3 jmetertodb.py -xmlfile ./Resume_Result.xml
+
+#Delete
+/home/jmeter/apache-jmeter-4.0/bin/jmeter -n -t /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Delete.jmx -l /home/jmeter/NCHC_Swagger/WL-A01/SOC/WL-A01_SOC_Delete.jtl
+
+python3 jmetertodb.py -xmlfile ./Delete_Result.xml
